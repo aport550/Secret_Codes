f7060c762afab0f8bda03663d76b03bb7a3313b3
Allan Porter <aport550@gmail.com>
2019-04-02 01:41:11 -0400

    First Commit

---
+++ secretcode/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+#lack of common sense username and password limits make this susceptible to overflows
+class User(models.Model):
+	username = models.CharField(max_length=1000)
+	password = models.CharField(max_length=1000)
+	secret = models.TextField()
+
+def __str__(self):
+	return self.username
+
+
+
---
+++ secretcode/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+import os
+from django.conf import settings
+from .models import User
+from django.views.decorators.csrf import csrf_exempt
+
+#landing page server
+def home(request):
+	return render(request, 'home.html')
+
+#insecure login functionality below
+@csrf_exempt #exempt CSRF protection to make this vulnerable
+def secret(request):
+    if request.method == "POST":
+    	print("THR")
+    	print(request)
+    	username = request.POST.get("name")
+    	password = request.POST.get("passwd")
+    	if username == 'admin':
+    		me = User.objects.get(username = username)
+    	else:
+    		return render(request, 'home.html')
+    	#very insecure manual login functionality, allows unlimited password attempts
+    	if password == me.password:
+    		template = loader.get_template('secret.html')
+    		context = {'secret': me.secret}
+    		return HttpResponse(template.render(context, request))
+    	else:
+    		return render(request, 'home.html')
+    else:
+    	return render(request, 'home.html')
+
+
+
+
+
+
